d687d9442c15d1e78fd3a75a01be97ea061edf2e
Luke Shumaker <lukeshu@datawire.io>
2022-03-02 12:53:59 -0700

    Try to avoid tripping firewalls
     - Allow specifying a max size
     - Set Content-Type to plaintext
     - Only stream valid ASCII characters
     - Set Content-Length if possible
     - Flush to avoid erroneously setting Content-Length if not possible

--- speedtest.go
+++ speedtest.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/datawire/dlib/dhttp"
 	"github.com/datawire/dlib/dlog"
@@ -21,12 +22,47 @@ func main() {
 	}
 }
 
+// asciiReader wraps an underlying io.Reader, and munges all read
+// bytes to be ASCII non-control characters.
+type asciiReader struct {
+	inner io.Reader
+}
+
+func (a asciiReader) Read(dat []byte) (int, error) {
+	n, err := a.inner.Read(dat)
+	for i := 0; i < n; i++ {
+		// the bit 8 is always zero for ascii
+		dat[i] &^= 0b1000_0000
+		// bits 6 and 7 are only ever clear for control
+		// characters, ensure one of them is set
+		if dat[i]&0b0110_0000 == 0 {
+			dat[i] |= 0b0100_0000
+		}
+	}
+	return n, err
+}
+
 func Main(ctx context.Context) error {
+
 	sc := &dhttp.ServerConfig{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := dlog.WithField(r.Context(), "request_id", fmt.Sprintf("%p", r))
+
+			dataSource := io.Reader(asciiReader{rand.Reader})
+			if size, err := strconv.ParseInt(r.URL.Query().Get("size"), 10, 0); err == nil && size > 0 {
+				w.Header().Set("Content-Length", strconv.FormatInt(size, 10))
+				dataSource = io.LimitReader(dataSource, size)
+			}
+
 			dlog.Infof(ctx, "begin: %s %s %s", r.Method, r.RequestURI, r.Proto)
-			n, _ := io.Copy(w, rand.Reader)
+
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			if flusher, ok := w.(http.Flusher); ok {
+				// don't buffer the request to auto-determine Content-Length
+				flusher.Flush()
+			}
+			n, _ := io.Copy(w, dataSource)
+
 			dlog.Infof(ctx, "end: wrote %d bytes", n)
 		}),
 	}
